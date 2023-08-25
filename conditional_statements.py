b672f3c27cf2e31c0d9cfb30e53326cebe2c43d2
rituparnadobey-dci <rituparna.dobey@dci-student.org>
2023-08-25 17:20:48 +0200

    Q_G

---
+++ conditional_statements.py
@@ -0,0 +1,14 @@
+# # Task 1 - calculate sum
+
+# num1 = int(input('Please enter 1st number: '))
+# num2 = int(input('Please enter 2nd number: '))
+# num3 = int(input('Please enter 3rd number: '))
+
+# if num1 == num2 == num3:
+#     sum_num = (num1 + num2 + num3) * 3
+#     print(sum_num,'is the total output')
+# else:
+#     sum_num = num1 + num2 + num3
+#     print('The total output is:',sum_num)
+
+
